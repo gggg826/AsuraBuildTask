84185c079686b1ba8738badbc0fb3d66252d4086
gggg826 <liushida@126.com>
2018-04-16 15:14:40 +0800

    optimiz

--- BuildAssetbundle.bat
+++ BuildAssetbundle.bat
@@ -1,10 +1,10 @@
-echo 姝ょ獥鍙ｈ嚜鍔ㄥ叧闂墠绂佹浜轰负鍏抽棴
+echo 此窗口自动关闭前禁止人为关闭
 echo.
 
-rem UNITY_5_5_4鐩綍
+rem UNITY_5_5_4目录
 set "UNITY_5_5_4_HOME=E:\Program Files\Unity5.5.4\Editor"
 
-rem 宸ョ▼鐩綍
+rem 工程目录
 set Client_Path=D:\Asura\trunk\Client
 "%UNITY_5_5_4_HOME%"\Unity.exe -batchmode -buildTarget Android -projectPath %Client_Path% -executeMethod CommandBuild.ExecuteBuild
 exit
