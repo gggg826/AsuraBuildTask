a72773f48b77dff8403a4468ac5505744289b01e
gggg826 <liushida@126.com>
2018-04-16 15:04:26 +0800

    Optimize

--- AsuraBuilder.bat
+++ AsuraBuilder.bat
@@ -6,19 +6,20 @@ rem Asura工程Trunk目录
 set Client_Path=D:\Asura\trunk\
 
 rem Table目录
-set Table_Path=%Client_Path%Public\
+set Table_Path=D:\Asura\trunk\Public\
 
 rem Assetbundle目录
-set Bundle_Path=%Client_Path%Client\Assets\StreamingAssets
-
-rem UNITY_5_5_4目录
-set "UNITY_5_5_4_HOME=E:\Program Files\Unity5.5.4f1\Editor"
+set Bundle_Path=D:\Asura\trunk\Client\Assets\StreamingAssets\
 
 REM 强行退出Unity编译器
 echo.
 echo --------------------------  Try Exit Unity --------------------------------------------
 start /wait %~dp0ForceQuitUnity.bat
 
+
+echo.
+echo --------------------------  SVN Cleanup -----------------------------------------------
+svn cleanup %Client_Path%
 
 echo.
 echo --------------------------  SVN Update ------------------------------------------------
@@ -30,24 +31,23 @@ svn revert --recursive %Client_Path%
 
 echo.
 echo --------------------------  Sync Table Data -------------------------------------------
-cd %Table_Path%
-start /wait %~dp0AysncForClientRun.bat
+cd /d %Table_Path%
+start /wait %Table_Path%AysncForClientRun.bat
 
 echo.
 echo --------------------------  Start Build AssetBundles ----------------------------------
 echo.
 echo --------------------------  Please Wait -----------------------------------------------
-cd %0
 start /wait %~dp0BuildAssetbundle.bat
 echo --------------------------  Build Completed -------------------------------------------
 
 
 echo.
 echo --------------------------  SVN Add ---------------------------------------------------
-cd %Bundle_Path%
-svn add
+cd /d %Bundle_Path%
+svn add . --force
 echo.
 echo --------------------------  SVN Commit ------------------------------------------------
-svn Commit -m "##### Auto build Commit #####"
+svn commit -m "##### Auto build Commit #####" *
 
 pause
--- BuildAssetbundle.bat
+++ BuildAssetbundle.bat
@@ -1,5 +1,10 @@
 echo 此窗口自动关闭前禁止人为关闭
 echo.
-set Client_Path=F:\UnityProject\Commercail\Asura\trunk\Client
+
+rem UNITY_5_5_4目录
+set "UNITY_5_5_4_HOME=E:\Program Files\Unity5.5.4\Editor"
+
+rem 工程目录
+set Client_Path=D:\Asura\trunk\Client
 "%UNITY_5_5_4_HOME%"\Unity.exe -batchmode -buildTarget Android -projectPath %Client_Path% -executeMethod CommandBuild.ExecuteBuild
 exit
