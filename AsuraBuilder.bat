595b5ee26003a546acbb6f7f699bcc6edbcc0735
gggg826 <liushida@126.com>
2018-04-16 16:06:20 +0800

    Optimize

--- AsuraBuilder.bat
+++ AsuraBuilder.bat
@@ -11,6 +11,12 @@ set Table_Path=D:\Asura\trunk\Public\
 rem Assetbundle目录
 set Bundle_Path=D:\Asura\trunk\Client\Assets\StreamingAssets\
 
+rem Apk目录
+set BuildApk_Path=D:\Asura\trunk\Client\Build\Asura_ANDROID\
+
+rem FTP目录
+set FTP_Path=D:\
+
 REM 强行退出Unity编译器
 echo.
 echo --------------------------  Try Exit Unity --------------------------------------------
@@ -49,5 +55,7 @@ svn add . --force
 echo.
 echo --------------------------  SVN Commit ------------------------------------------------
 svn commit -m "##### Auto build Commit #####" *
+
+xcopy %BuildApk_Path%*.apk %FTP_Path% /c /i /y
 
 pause
